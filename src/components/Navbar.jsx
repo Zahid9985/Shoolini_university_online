fce8061d977e97e497fdba87a9e39257cc6a2397
Zahid <mondalz493@gmail.com>
2026-02-26 01:03:08 +0530

    Fix nav image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import PressButton from './PressButton';
 import '../styles/styles.css';
+import navImg from "../assets/logo.png"
 
 const Navbar = ({ onApplyClick }) => {
   return (
     <nav className="navbar">
       <div className="container">
         <div className="navbar-logo">
-          <img src="src\assets\logo.png" alt="Online Shoolini Logo" className="navbar-logo-img" />
+          <img src={navImg} alt="Online Shoolini Logo" className="navbar-logo-img" />
         </div>
         <div className="navbar-cta">
           <PressButton className="btn-primary" onClick={onApplyClick}>
